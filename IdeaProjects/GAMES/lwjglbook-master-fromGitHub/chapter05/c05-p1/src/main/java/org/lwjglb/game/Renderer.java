8107ea09da1f2e87a2518678d6e66b870859eb7c
MaxElmanov <89106232571@yandex.ru>
2019-02-26 15:39:06 +0300

    first commit

---
+++ IdeaProjects/GAMES/lwjglbook-master-fromGitHub/chapter05/c05-p1/src/main/java/org/lwjglb/game/Renderer.java
@@ -0,0 +1,56 @@
+package org.lwjglb.game;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+import org.lwjglb.engine.Utils;
+import org.lwjglb.engine.Window;
+import org.lwjglb.engine.graph.Mesh;
+import org.lwjglb.engine.graph.ShaderProgram;
+
+public class Renderer {
+
+    private ShaderProgram shaderProgram;
+
+    public Renderer() {
+    }
+
+    public void init() throws Exception {
+        shaderProgram = new ShaderProgram();
+        shaderProgram.createVertexShader(Utils.loadResource("/vertex.vs"));
+        shaderProgram.createFragmentShader(Utils.loadResource("/fragment.fs"));
+        shaderProgram.link();
+    }
+
+    public void clear() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
+
+    public void render(Window window, Mesh mesh) {
+        clear();
+
+        if (window.isResized()) {
+            glViewport(0, 0, window.getWidth(), window.getHeight());
+            window.setResized(false);
+        }
+
+        shaderProgram.bind();
+
+        // Draw the mesh
+        glBindVertexArray(mesh.getVaoId());
+        glEnableVertexAttribArray(0);
+        glDrawElements(GL_TRIANGLES, mesh.getVertexCount(), GL_UNSIGNED_INT, 0);
+
+        // Restore state
+        glDisableVertexAttribArray(0);
+        glBindVertexArray(0);
+
+        shaderProgram.unbind();
+    }
+
+    public void cleanup() {
+        if (shaderProgram != null) {
+            shaderProgram.cleanup();
+        }
+    }
+}
