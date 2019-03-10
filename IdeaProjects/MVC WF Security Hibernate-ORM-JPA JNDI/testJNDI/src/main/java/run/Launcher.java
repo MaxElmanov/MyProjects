6ae4399d92e3c299b212d8377d3750782ea50cdc
MaxElmanov <89106232571@yandex.ru>
2019-03-10 20:26:06 +0300

    jndi bind undind functions

--- IdeaProjects/MVC WF Security Hibernate-ORM-JPA JNDI/testJNDI/src/main/java/run/Launcher.java
+++ IdeaProjects/MVC WF Security Hibernate-ORM-JPA JNDI/testJNDI/src/main/java/run/Launcher.java
@@ -2,46 +2,32 @@ package run;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import run.objects.Monkey;
 
-import javax.naming.*;
 import javax.naming.Context;
-import javax.security.auth.Refreshable;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.naming.Referenceable;
 import java.util.Hashtable;
-import java.util.Objects;
 
-public class Launcher implements Referenceable {
+public class Launcher{
 
     public static final Logger logger = LogManager.getLogger(Referenceable.class.getName());
 
-    private String name = "it's my super launcher";
-
     public static void main(String[] args) throws NamingException {
 
-        String name = "C:\\Users\\MaxNick\\IdeaProjects\\MVC WF Security Hibernate-ORM-JPA JNDI\\testJNDI\\src\\main\\resources\\text.txt";
-
-        Hashtable<String, String> environment =  new Hashtable<String, String>();
+        Hashtable<String, String> environment = new Hashtable<>();
         environment.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.fscontext.RefFSContextFactory");
-        environment.put(Context.PROVIDER_URL, "file:/D:/");
+        environment.put(Context.PROVIDER_URL, "file:/D:/Repository/My projects (programing)/MyProjects.git/IdeaProjects/MVC WF Security Hibernate-ORM-JPA JNDI/testJNDI/src/main/resources");
 
         Context context = new InitialContext(environment);
 
-        try {
-            Object launcher = context.lookup("Launcher");
-            logger.info("\n---------\n"+launcher+"\n----------");
-            if(Objects.nonNull(launcher)){
-                logger.info(((Launcher) launcher).name);
-            }
-        }
-        catch (Exception e){
-            logger.error("NameAlreadyBoundException " + e.getMessage());
-        }
-    }
+        Monkey monkeySteven = new Monkey("Steven", "orange", true);
+        Monkey monkeyAlex = new Monkey("Alex", "tomato", false);
 
-    public Reference getReference() throws NamingException {
-        Reference reference = new Reference(Launcher.class.getName());
+        context.rebind("monkeySteven", monkeySteven);
+        context.rebind("monkeyAlex", monkeyAlex);
 
-        reference.add(new StringRefAddr("name", this.name));
-
-        return reference;
+        context.unbind("monkeySteven");
     }
 }
