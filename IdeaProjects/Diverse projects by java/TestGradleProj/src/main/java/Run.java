692591c35e10c57a9d8460f7c46c3db1f98c6a50
MaxElmanov <89106232571@yandex.ru>
2019-07-03 21:10:04 +0300

    03.07.19

--- IdeaProjects/Diverse projects by java/TestGradleProj/src/main/java/Run.java
+++ IdeaProjects/Diverse projects by java/TestGradleProj/src/main/java/Run.java
@@ -1,13 +1,6 @@
 public class Run
 {
-    public static void main(String[] args)
-    {
-        try {
-            Thread.sleep(1000);
-        }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Hello");
+    public String getMessage(){
+        return "2 b || ! 2 b";
     }
 }
